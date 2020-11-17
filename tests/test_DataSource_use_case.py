7301946a53131486e4a048b0882a7de8cfa0d512
Richard Parke <richardparke@Richards-MacBook-Air.local>
2020-11-17 23:43:03 +0000

    Successful refactor of tests using dictionary to mock hdf5 file

--- tests/test_DataSource_use_case.py
+++ tests/test_DataSource_use_case.py
@@ -6,61 +6,18 @@ import os
 from nexus_iterator import DataSource
 from nexus_iterator import KeyFollower
 from unittest.mock import MagicMock, Mock, patch
-
-
-#Creating datasets from numpy arrays
-class DataSet(np.ndarray):pass
-
-def create_dataset_from_numpy_array(numpy_array):
-    ds = DataSet(numpy_array.shape)
-    ds[:] = numpy_array[:]
-    ds.refresh = lambda:None
-    return ds
-
-#Create a dataset of keys that is completely filled with non-zero values
-#Number of iterations = 50
-complete_dataset_data = create_dataset_from_numpy_array(np.arange(50*10).reshape(5,10,1,10))
-complete_dataset_keys = create_dataset_from_numpy_array(np.arange(50).reshape(5,10,1,1)+1)
-
-#Number of iterations = 40
-incomplete_dataset_data = complete_dataset_data.copy()
-incomplete_dataset_data[-1] = 0
-incomplete_dataset_data = create_dataset_from_numpy_array(incomplete_dataset_data)
-incomplete_dataset_keys = complete_dataset_keys.copy()
-incomplete_dataset_keys[-1] = 0
-incomplete_dataset_keys = incomplete_dataset_keys.reshape(complete_dataset_keys.shape)
-incomplete_dataset_keys = create_dataset_from_numpy_array(incomplete_dataset_keys)
-
-
-four_dimensional_dataset_data = complete_dataset_data.copy()
-four_dimensional_dataset_data = create_dataset_from_numpy_array(four_dimensional_dataset_data)
-
-four_dimensional_dataset_keys = complete_dataset_keys.copy()
-four_dimensional_dataset_keys = create_dataset_from_numpy_array(four_dimensional_dataset_keys)
-
-
-three_dimensional_dataset_data = complete_dataset_data.copy()
-three_dimensional_dataset_data = three_dimensional_dataset_data.reshape(50,1,10)
-three_dimensional_dataset_data = create_dataset_from_numpy_array(three_dimensional_dataset_data)
-
-three_dimensional_dataset_keys = complete_dataset_keys.copy()
-three_dimensional_dataset_keys = three_dimensional_dataset_keys.reshape(50,1,1)
-three_dimensional_dataset_keys = create_dataset_from_numpy_array(three_dimensional_dataset_keys)
-
-
-
+import DataSourceDatasets as Dataset
 
 
 
 def test_iterates_complete_dataset():
 
       
-      h5py.File = MagicMock()
-      h5py.File.return_value = {"keys": {'complete':complete_dataset_keys}, "data": {"complete": complete_dataset_data}}
+      f = {"keys": {'complete':Dataset.complete_dataset_keys()}, 
+                                "data": {"complete": Dataset.complete_dataset_data()}}
       
       data_paths = ['data']
       key_paths = ['keys']
-      f = h5py.File()
       df = DataSource.DataFollower(f, key_paths, data_paths, timeout = 1)
       current_key = 0
       for dset in df:
@@ -70,12 +27,12 @@ def test_iterates_complete_dataset():
 def test_iterates_incomplete_dataset():
 
       
-      h5py.File = MagicMock()
-      h5py.File.return_value = {"keys": {'incomplete':incomplete_dataset_keys}, "data": {"incomplete": incomplete_dataset_data}}
+
+      f = {"keys": {'incomplete':Dataset.incomplete_dataset_keys()}, 
+           "data": {"incomplete": Dataset.incomplete_dataset_data()}}
       
       data_paths = ['data']
       key_paths = ['keys']
-      f = h5py.File()
       df = DataSource.DataFollower(f, key_paths, data_paths, timeout = 1)
       current_key = 0
       for dset in df:
@@ -86,15 +43,13 @@ def test_iterates_incomplete_dataset():
 def test_iterates_multiple_incomplete_dataset():
 
       
-      h5py.File = MagicMock()
-      h5py.File.return_value = {"keys": {'complete':complete_dataset_keys,
-                                         'incomplete':incomplete_dataset_keys}, 
-                                "data": {"complete": complete_dataset_data,
-                                    "incomplete": incomplete_dataset_data}}
+      f = {"keys": {'complete':Dataset.complete_dataset_keys(),
+                                         'incomplete':Dataset.incomplete_dataset_keys()}, 
+                                "data": {"complete": Dataset.complete_dataset_data(),
+                                    "incomplete": Dataset.incomplete_dataset_data()}}
       
       data_paths = ['data']
       key_paths = ['keys']
-      f = h5py.File()
       df = DataSource.DataFollower(f, key_paths, data_paths, timeout = 1)
       current_key = 0
       for dset in df:
@@ -108,17 +63,15 @@ def test_iterates_multiple_incomplete_dataset():
 
 #Check that the correct dataset is returned ignoring shapes
 def test_correct_return_data_complete():
-      h5py.File = MagicMock()
-      h5py.File.return_value = {"keys": {'complete':complete_dataset_keys}, 
-                                "data": {"complete": complete_dataset_data}}
+      f = {"keys": {'complete':Dataset.complete_dataset_keys()}, 
+                                "data": {"complete": Dataset.complete_dataset_data()}}
       data_paths = ['data']
       key_paths = ['keys']
-      f = h5py.File()
       df = DataSource.DataFollower(f, key_paths, data_paths, timeout = 1)
       full_dataset = np.array([])
       for dset in df:
           full_dataset = np.concatenate((full_dataset, dset[0].flatten()))
-      assert(complete_dataset_data.flatten() == full_dataset.flatten()).all()
+      assert(Dataset.complete_dataset_data().flatten() == full_dataset.flatten()).all()
       
 
 
@@ -127,15 +80,13 @@ def test_correct_return_data_complete():
 
 
 def test_correct_return_shape():
-    h5py.File = MagicMock()
-    h5py.File.return_value = {"keys":{"four_dimensional": four_dimensional_dataset_keys,
-                                      "three_dimensional": three_dimensional_dataset_keys},
-                              "data":{"four_dimensional": four_dimensional_dataset_data,
-                                      "three_dimensional": three_dimensional_dataset_data}}
+    f = {"keys":{"four_dimensional": Dataset.four_dimensional_dataset_keys(),
+                                      "three_dimensional": Dataset.three_dimensional_dataset_keys()},
+                              "data":{"four_dimensional": Dataset.four_dimensional_dataset_data(),
+                                      "three_dimensional": Dataset.three_dimensional_dataset_data()}}
     
     data_paths = ['data']
     key_paths = ['keys']
-    f = h5py.File()
     df = DataSource.DataFollower(f, key_paths, data_paths, timeout = 1)
     for dset in df:
         assert dset[0].shape == (1,1,1,10) and dset[1].shape == (1,1,10)
@@ -143,12 +94,11 @@ def test_correct_return_shape():
 
             
 def test_reset_method_iterates_correct_length():
-      h5py.File = MagicMock()
-      h5py.File.return_value = {"keys": {'complete':complete_dataset_keys}, "data": {"complete": complete_dataset_data}}
+      f = {"keys": {'complete':Dataset.complete_dataset_keys()}, 
+           "data": {"complete": Dataset.complete_dataset_data()}}
       
       data_paths = ['data']
       key_paths = ['keys']
-      f = h5py.File()
       df = DataSource.DataFollower(f, key_paths, data_paths, timeout = 1)
       current_key = 0
       for dset in df:
