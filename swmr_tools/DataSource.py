f5a4b242380e94ff9a27196ac4940de25f4caf66
Richard Parke <richardparke@Richards-MacBook-Air.local>
2020-12-11 13:06:24 +0000

    Added docstrings to DataSource.py

--- swmr_tools/DataSource.py
+++ swmr_tools/DataSource.py
@@ -5,7 +5,42 @@ from swmr_tools.KeyFollower import Follower, FrameGrabber
 
 
 class DataFollower():
+    """Iterator for returning dataset frames for any number of datasets. This
+    class acts as a wrapper for the KeyFollower.Follower and 
+    KeyFollower.FrameGrabber classes.
     
+    Parameters
+    ----------
+    
+    hdf5_file: h5py.File
+        Instance of h5py.File object. Choose the file containing data you wish
+        to follow.
+        
+    keypaths: list
+        A list of paths (as strings) to groups in hdf5_file containing unique
+        key datasets. (Note: paths must be to the group containing the dataset
+                       and not full paths to the dataset itself)
+        
+    dataset_paths: list
+        A list of paths (as strings) to datasets in hdf5_file that you wish
+        to return frames from (Note: paths must be to the dataset and not
+                               to the group containing it)
+        
+    timeout: int (optional)
+        The maximum time allowed for a dataset to update before the timeout
+        termination condition is trigerred and iteration is halted. If a value
+        is not set this will default to 10 seconds.
+        
+        
+    Examples
+    --------
+    
+    >>> with h5py.File("/home/documents/work/data/example.h5", "r", swmr = True) as f:
+    >>>     df = DataFollower(f, path_to_key_group, path_to_datasets, timeout = 1)
+    >>>     for list_of_frames in df:
+    >>>         print(list_of_frames)
+    
+    """
     def __init__(self, hdf5_file, keypaths, dataset_paths, timeout = 1):
         self.hdf5_file = hdf5_file
         self.dataset_paths = dataset_paths
@@ -43,6 +78,8 @@ class DataFollower():
         
         
     def reset(self):
+        '''Reset the iterator to start again from frame 0
+        '''
         self.kf.reset()
         
         
